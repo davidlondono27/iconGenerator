216ef22dacd8d8d0cdb8d1266ce21b105139e07c
David <hlc.david327@gmail.com>
2022-04-21 09:51:09 -0500

    Initial Commit

---
+++ Shared/IconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconGeneratorApp.swift
+//  Shared
+//
+//  Created by David Londoño on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct IconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
